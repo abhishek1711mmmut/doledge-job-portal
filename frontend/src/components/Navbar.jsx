36d3aa9a825e2796d1fa6caa1ca52870f1a62988
abhishek17111mmmut <abhishek1711mmmut@gmail.com>
2024-01-17 01:37:35 +0530

    hamburger menu added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import { a } from 'react-router-dom';
 import './css/Navbar.css';
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
 import userLogo from '../images/userLogo.png'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const ref=useRef();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  const navClass = isOpen ? 'block' : 'hidden';
+  const overlay=<div className={`fixed ${isOpen ? "block" : "hidden"} w-full h-full top-0 bottom-0 left-0 right-0 bg-[rgba(0,0,0,0.5)] z-[2] cursor-pointer`}></div>
+
+  const dropdown = 
+  <div className='flex flex-col gap-2 w-full py-5 px-3'>
+    <div className='flex gap-2 gap-y-3 w-full'>
+      <button className='bg-[#15AA6A] text-white opacity-90 px-3 py-1 rounded-lg flex justify-center items-center w-full' onClick={() => setIsOpen(false)}>
+        Sign In
+      </button>
+      <button className='bg-[#F58634] text-white opacity-90 px-3 py-1 rounded-lg flex justify-center items-center w-full' onClick={() => setIsOpen(false)}>
+        Register
+      </button>
+    </div>
+    <button className='bg-[#bf5157] text-white rounded-lg px-3 py-1 flex justify-center items-center' onClick={() => setIsOpen(false)}>
+      Recruiter Login
+    </button>
+    <div className='flex flex-col gap-3 text-black font-semibold w-full mx-auto'>
+      <Link to='/' className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Home
+      </Link>
+      <Link to='/abc' className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Jobs
+      </Link>
+      <Link className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Services
+      </Link>
+      <Link className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Companies
+      </Link>
+      <Link className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Blogs
+      </Link>
+      <Link className='border border-black/50 rounded-md py-1' onClick={() => setIsOpen(false)}>
+        Contact Us
+      </Link>
+    </div>
+  </div>
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (
+        isOpen &&
+        ref.current &&
+        !ref.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', handler);
+    };
+  }, [isOpen]);
 
   return (
     <nav>
-      <div className='flex justify-between items-center pt-2 pb-4 w-[95%] mx-auto'>
-        <img src={Logo} alt="" className='w-24 lg:w-32 xl:w-40'/>
+      <div className='relative flex justify-between items-center pt-2 pb-4 w-[95%] mx-auto'>
+        <Link to='/' className='flex justify-center items-center'>
+        <img src={Logo} alt="" className='w-24 lg:w-32 xl:w-40' />
+        </Link>
         <div className='hidden md:flex gap-x-3 justify-between items-center lg:gap-4 xl:gap-x-8 md:text-xs lg:text-base xl:text-xl font-light' style={{fontFamily:'Poppins'}}>
             <Link to='/'>
               HOME
@@ -38,7 +95,7 @@ const Navbar = () => {
               CONTACT US
             </Link>
         </div>
-        <div className='flex justify-between items-center gap-1 lg:gap-3'>
+        <div className='hidden md:flex justify-between items-center gap-1 lg:gap-3'>
           <div className='flex gap-x-2 text-xs lg:text-base font-bold text-white'>
             <button className='bg-[#15AA6A] opacity-90 px-2 py-1 lg:px-3 lg:py-1 xl:px-5 xl:py-2 rounded-2xl flex justify-center items-center'>
               Sign In
@@ -55,95 +112,22 @@ const Navbar = () => {
             </p>
           </button>
         </div>
+        {overlay}
       </div>
+      {
+          !isOpen
+          && 
+          <div className='absolute md:hidden right-5 top-3 border-2 border-black/50 p-1 rounded-lg'>
+            <GiHamburgerMenu className='text-3xl cursor-pointer' onClick={toggleMenu}/>
+          </div>
+        }
+        
+          <div ref={ref} className={`absolute w-[70%] ${isOpen ? "scale-x-100" : "scale-x-0"} origin-right transition-transform ease-in-out duration-300 right-0 top-0 flex flex-col justify-center items-center bg-gray-100 p-2 rounded-lg rounded-tr-none z-10 border border-black`}>
+            <RxCross2 className='md:hidden text-4xl mr-3 mt-2 self-end cursor-pointer' onClick={toggleMenu}/>
+             {dropdown}
+          </div>
+          : 
     </nav>
-  //   <nav className="w-[100vw] flex items-center justify-center h-[10vw] shadow-md ">
-  //     <div className="container mx-auto flex items-center justify-between">
-  //     {/* <Link to="/" className="navbar-brand w-[80px] h-[20px] ">
-  // <img src={'/src/images/logo.png'} alt="Logo" />
-  //      </Link> */}
-
-  //       <div className="lg:hidden">
-  //         <button className="text-stone-950 text-2xl" onClick={toggleMenu}>
-  //           &#8801;
-  //         </button>
-  //       </div>
-
-  //       <ul className={`lg:flex ${navClass} space-x-8 text-stone-950 font-light justify-center mt-3`}>
-  //         <li>
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px] transition">
-  //             HOME
-  //           </Link>
-  //         </li>
-  //         <li>
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             SERVICES
-  //           </Link>
-  //         </li>
-  //         <li>
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             JOBS
-  //           </Link>
-  //         </li>
-  //         <li>
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             COMPANIES
-  //           </Link>
-  //         </li>
-  //         <li className='w-[7vw]' >
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             ABOUT US
-  //           </Link>
-  //         </li>
-  //         <li>
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             BLOGS
-  //           </Link>
-  //         </li>
-
-  //         <li className='w-[10vw]' >
-  //           <Link to="#" className="hover:text-emerald-600 text-[13px]  transition">
-  //             CONTACT US
-  //           </Link>
-  //         </li>
-
-  //         <li className='w-[12vw]'>
-           
-  //           <a className="Signin text-[13px] bg-emerald-600 text-black hover:bg-emerald-700  ml-12 py-2 px-4 rounded-full cursor-pointer transition">
-  //             Sign In
-          
-  //         </a>
-  //         </li>
-
-  //         <li  className='h-[fit-content] ' > 
-           
-  //           <a className="register text-[13px]   bg-orange-600 text-black hover:bg-emerald-700 py-2 px-4 rounded-full cursor-pointer transition">
-  //             Register
-          
-  //         </a>
-  //         </li>
-
-  //         <li>
-  //           <Link to='#'>
-
-  //           <div className="flex space-x-3 mb-3 " >
-  //             <div className="w-1 h-[6vw] bg-black bg-opacity-20" />
-  //             <div className='w-[7.5vw] ' >
-  //               <img
-  //                 loading="lazy"
-  //                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/eee71dca-fdd5-4fab-9e98-78d30d543d80?apiKey=98c2e51ab1a04825837c41320a839326&"
-  //                 alt="Employer"
-  //                 className="w-full h-10"
-
-  //                 />
-  //               <div className="text-stone-950 text-[10px]  font-light">Employer Login</div>
-  //             </div>
-  //           </div>
-  //                 </Link>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   </nav>
   );
 };
 
